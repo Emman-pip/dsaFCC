299c285d2bdaf9cbfdbabd34b1c99efeed2eb197
Emman-pip <115330041+Emman-pip@users.noreply.github.com>
2023-12-29 10:18:16 +0800

    Add the main game logic.

--- DSAFiles/wordGame.h
+++ DSAFiles/wordGame.h
@@ -2,32 +2,74 @@
 #include <iostream>
 #include <string>;
 #include <cstdlib>
+#include <time.h>
 
 using namespace std;
 
 class wordGame
 {
 private:
-	string lol[3] = { "hello", "world", "guess" };
+	string lol[6] = { "hello", "world", "guess", "pogi", "emman", "hehe"};
 	string randomWord;
+	int tries = 13;
+	string name;
 public:
 	wordGame() 
 	{
+		srand(time(NULL));
 		const int number = rand() % (sizeof(this->lol) / sizeof(string));
-		cout << this->lol[number] << endl;
 		randomWord = this->lol[number];
 	}
-
 	void game()
 	{
+		string line = "-";
+		int lenOfWord = this->randomWord.length();
+		string word;
 
+		for (int i = 0; i < lenOfWord; i++)
+		{
+			word += line;
+		}
+		char guess = ' ';
+
+
+		while (true)
+		{
+			if (tries == 0)
+			{
+				cout << "You lost!";
+				return;
+			}
+			cout << " Guess the word. (" << this->tries << " tries left)" << endl;
+			for (int i = 0; i < lenOfWord; i++)
+			{
+				if (guess == this->randomWord[i])
+				{
+					word[i] = guess;
+				}
+				cout << word[i] << endl;
+			}
+
+			cout << "\n";
+			
+			if (this->randomWord == word)
+			{
+				break;
+			}
+			cout << "Enter a character: ";
+			cin >> guess;
+			this->tries--;
+		}
+		cout << "Congratulations, " << this->name << ". You won!" <<  endl;
 	}
+
 	void display() 
 	{
-		string name;
-		cout << "\nWhat is your name?";
-		cin >> name;
-		cout << "Goodluck, " << name << "!" << endl;
+
+		cout << "\nWhat is your name? ";
+		cin >> this->name;
+		cout << "Goodluck, " << this->name << "!" << endl;
+		game();
 	}
 	
 };
